0b914bfdb5ef79867c85e2380c14efcd934d8d47
Teekay <tk.kayas44@gmail.com>
2025-07-19 13:59:59 +0200

    Fix typo in author model name and update ForeignKey reference in Book model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class aurthor(models.Model):
+class author(models.Model):
     name = models.CharField(max_length=100)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(aurthor, on_delete=models.CASCADE)
+    author = models.ForeignKey(author, on_delete=models.CASCADE)
 
 class library(models.Model):
     name = models.CharField(max_length=100)
