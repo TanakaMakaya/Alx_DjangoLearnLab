ad630b359e8611f5dc37d74f3f08a6f0007d1f11
Teekay <tk.kayas44@gmail.com>
2025-07-19 14:38:54 +0200

    vvv

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 # Function-based view for listing books
 def list_books(request):
