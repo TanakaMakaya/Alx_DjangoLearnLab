398def0b91273f6000b5eb59dc8c51c1a77441d7
Teekay <tk.kayas44@gmail.com>
2025-07-20 20:47:08 +0200

    qqqq

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     # ✅ Built-in Django auth views
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
     # ✅ Your custom register view
     path('register/', views.register_view, name='register'),
