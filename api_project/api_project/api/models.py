55e7736bb8edd5e196623b73c034e464b3561651
Teekay <tk.kayas44@gmail.com>
2025-07-31 13:28:24 +0200

    xzz

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -2,7 +2,7 @@ from django.db import models # type: ignore
 
 # Create your models here.
 
-class book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
 
