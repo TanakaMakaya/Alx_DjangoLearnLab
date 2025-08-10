a898e30f30b8e11216f1214ff134cf8fffa4699d
Teekay <tk.kayas44@gmail.com>
2025-08-10 21:41:15 +0200

    jkj

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,6 +6,13 @@ class BookList(generics.ListAPIView):
     """
     API view to list all Book instances.
     Uses BookSerializer to serialize the data.
+    """
+    queryset = Book.objects.all() # Defines the set of objects to retrieve
+    serializer_class = BookSerializer # Specifies the serializer to use
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for CRUD operations on Book model.
     """
     queryset = Book.objects.all() # Defines the set of objects to retrieve
     serializer_class = BookSerializer # Specifies the serializer to use
